e1b901a6349ddb918639153788903faba33ce6e4
Richard Carmo Lisboa <andreasmora75@gmail.com>
2024-04-25 17:49:37 -0300

    java

---
+++ src/com/java/Variaveis.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 48;
+        String nome = "Andre";
+        String nomeCachorro = "Jansen";
+
+        System.out.println("Sua idade e: " + idade);
+        System.out.println("Seu nome e: " + nome);
+        System.out.println("Nome do seu cachorro e: " + nomeCachorro);
+
+
+    }
+}
